94a69671d8ee2471d36f7fe9628a56bca9af56d4
Tyler Hooks <tylerhooks8@gmail.com>
2020-07-11 00:07:36 -0500

    Finished Project

---
+++ js/script.js
@@ -0,0 +1,18 @@
+var  activeMenuMobile= document.getElementById("activeMenuMobile");
+var shareButtonMobile = document.getElementById("shareButtonMobile");
+
+var activeButton = document.getElementById("activeButton");
+shareButtonMobile.addEventListener("click", shareButtonClicked); 
+activeButton.addEventListener("click", activeButtonClicked);
+
+
+function shareButtonClicked () {
+   if(activeMenuMobile.classList.contains("d-none")) {
+    activeMenuMobile.classList.remove("d-none");
+   } else {
+       activeMenuMobile.classList.add("d-none");
+   }
+}
+function activeButtonClicked () {
+    activeMenuMobile.classList.add("d-none");
+}
