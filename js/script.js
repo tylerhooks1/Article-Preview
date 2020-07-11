e79efb52d63a02ab0f5a4493c4cf38aad17901e5
Tyler Hooks <tylerhooks8@gmail.com>
2020-07-11 00:33:54 -0500

    final touches

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
-var  activeMenuMobile= document.getElementById("activeMenuMobile");
+var activeMenuMobile = document.getElementById("activeMenuMobile");
 var shareButtonMobile = document.getElementById("shareButtonMobile");
 
 var activeButton = document.getElementById("activeButton");
-shareButtonMobile.addEventListener("click", shareButtonClicked); 
+shareButtonMobile.addEventListener("click", shareButtonClicked);
 activeButton.addEventListener("click", activeButtonClicked);
 
 
-function shareButtonClicked () {
-   if(activeMenuMobile.classList.contains("d-none")) {
-    activeMenuMobile.classList.remove("d-none");
-   } else {
-       activeMenuMobile.classList.add("d-none");
-   }
+function shareButtonClicked() {
+    if (activeMenuMobile.classList.contains("d-none")) {
+        activeMenuMobile.classList.remove("d-none");
+    } else {
+        activeMenuMobile.classList.add("d-none");
+    }
 }
-function activeButtonClicked () {
+function activeButtonClicked() {
     activeMenuMobile.classList.add("d-none");
 }
